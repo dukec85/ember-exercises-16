f33aeff650a6181cd7c36d54e7bbf4296a499348
Christopher Duke <cd.duke@gmail.com>
2016-07-20 16:02:00 -0500

    a few things

---
+++ app/routes/blogs.js
@@ -0,0 +1,9 @@
+
+mport Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return fetch('https://tiny-tn.herokuapp.com/collections/blogs')
+      .then((res) => res.json());
+  }
+});
