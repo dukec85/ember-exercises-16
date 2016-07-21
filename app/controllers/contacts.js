804a4f66d23a7022076a2f631987a26277fc2ecf
Christopher Duke <cd.duke@gmail.com>
2016-07-21 16:10:47 -0500

    fix

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -24,20 +24,17 @@ export default Ember.Controller.extend({
       this.toggleProperty('isShowingForm');
     },
 
-    deleteContact() {
-      const data = {
-      };
-
-      fetch('https://tiny-tn.herokuapp.com/collections/cd-contacts', {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        },
-        method: 'delete'}).then((res) => res.json()).then(() => {
-          var todo = this.get('model');
-          todo.deleteRecord();
-          todo.save();
+    deleteContact(person) {
+      if (confirm('Are you sure you want to delete?')) {
+        fetch('https://tiny-tn.herokuapp.com/collections/cd-contacts' + person._id, {
+          method: 'Delete',
+        }).then(() => {
+          const updatedList = this.model.filter((item) => {
+            return item._id !== person._id;
+          });
+          this.set('model', updatedList);
         })
-    }
+      }
+    },
   }
 });
